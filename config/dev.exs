06bae72b0692946dadbde5afd596521ad8d364cb
Paolo D'Incau <paolo.dincau@gmail.com>
2016-12-11 10:57:00 +0100

    remove username and password for postgres in dev/test

--- config/dev.exs
+++ config/dev.exs
@@ -24,8 +24,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :todo_api, TodoApi.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "todo_api_dev",
   hostname: "localhost",
   pool_size: 10
