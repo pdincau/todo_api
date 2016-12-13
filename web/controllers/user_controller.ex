4d042d8051c8e82bde56489c7da57f65d80c0004
Paolo D'Incau <paolo.dincau@gmail.com>
2016-12-13 20:15:46 +0100

    remove unused code

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -7,8 +7,6 @@ defmodule TodoApi.UserController do
     attrs = %{email: params["email"]}
     {:ok, user} = Users.Invite.call(attrs)
 
-    {:ok, body} = %{"data" => %{id: user.id, email: user.email}} |> Poison.encode
-
     conn
       |> put_status(:created)
       |> render("show.json", user: user)
