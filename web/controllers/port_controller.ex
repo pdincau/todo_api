0c22264c44a21921675b3e96eb680cef1385bd78
Paolo D'Incau <paolo.dincau@gmail.com>
2016-12-22 09:30:21 +0100

    removed commented code

--- web/controllers/port_controller.ex
+++ web/controllers/port_controller.ex
@@ -6,18 +6,6 @@ defmodule TodoApi.PortsController do
     {:ok, body} = %{} |> Poison.encode
     conn |> resp(201, body)
   end
-
-  ##  def update(conn, %{"calibration_rate" => calibration_rate}) do
-    ##  case Complex.update_calibration(calibration_rate) do
-        ##{:ok, new_calibration} ->
-          ##{:ok, body} = %{"calibration" => new_calibration} |> Poison.encode
-          ##{:ok, body} = %{"calibration" => new_value} |> Poison.encode
-          ##conn |> resp(200, body)   
-        ##{:error, "nasty error here"} ->
-          ## conn |> resp(404, body)   
-          ##....
-        ##end
-        ##end
   
   def show(conn, %{"id" => id}) do
     value = Complex.foo(2)
